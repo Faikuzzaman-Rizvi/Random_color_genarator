d1140b16f15b2c05e66f685a667aeddd15959455
Rizvi <150575416+Faikuzzaman-Rizvi@users.noreply.github.com>
2024-03-09 14:19:22 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-// console.log('DOM')
+console.log('DOM')
 
-// let boxes = document.querySelector(".container").children
+let boxes = document.querySelector(".container").children
 
 
 
-// function getRandomcolor(){
-//   let val1 = Math.ceil(0 + Math.random()* 255);
-//   let val2 = Math.ceil(0 + Math.random()* 255);
-//   let val3 = Math.ceil(0 + Math.random()* 255);
-//   return  `rgb(${val1}, ${val2}, ${val3})`
-// }
-// Array.from(boxes).forEach(e=>{
-//   e.style.backgroundColor = getRandomcolor()
-//   e.style.color = getRandomcolor()
-// })
+function getRandomcolor(){
+  let val1 = Math.ceil(0 + Math.random()* 255);
+  let val2 = Math.ceil(0 + Math.random()* 255);
+  let val3 = Math.ceil(0 + Math.random()* 255);
+  return  `rgb(${val1}, ${val2}, ${val3})`
+}
+Array.from(boxes).forEach(e=>{
+  e.style.backgroundColor = getRandomcolor()
+  e.style.color = getRandomcolor()
+})
